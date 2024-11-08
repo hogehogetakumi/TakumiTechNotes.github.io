e9d82ae2fc15fd89c6edc51c12f5a6ec3dc5066c
佐々木巧 <norikondakayoubi@gmail.com>
2024-11-08 19:03:53 +0900

    options vs composition

--- docs/config.js
+++ docs/config.js
@@ -10,7 +10,7 @@ module.exports = {
         '/',
         '/about/',
         '/posts/first-post',
-        '/posts/options_composition'
+        '/posts/options-vs-composition-api'
       ]
     }
   }
